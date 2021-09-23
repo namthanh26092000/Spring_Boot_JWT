a9fbb770966ce75cca2cd767352aca73eadc7130
Nam Bui <buithanhnam26092000@gmail.com>
2021-09-23 18:51:18 +0700

    commit and push

---
+++ src/main/java/com/example/spring_boot_jwt_btn/entity/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.example.spring_boot_jwt_btn.entity;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String deleted;
+
+    @CreatedDate
+    private Date createdAt;
+
+    @LastModifiedDate
+    private Date updatedAt;
+
+    @CreatedBy
+    private Long createdBy;
+
+    @LastModifiedBy
+    private Long updatedBy;
+
+}
--- src/main/java/com/example/spring_boot_jwt_btn/entity/User.java
+++ src/main/java/com/example/spring_boot_jwt_btn/entity/User.java
@@ -1,32 +1,23 @@
 package com.example.spring_boot_jwt_btn.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
-import lombok.*;
+import java.util.HashSet;
+import java.util.Set;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@Entity
-@Table(name = "studentjpa")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+    @Entity
+    @Table(name = "t_user")
+    @Getter
+    @Setter
+    public class User extends BaseEntity {
+        private String username;
+        private String password;
 
-    @Column(name = "name")
-    private String name;
-
-    @Column(name = "age")
-    private int age;
-
-    @Column(name = "address")
-    private String address;
-
-    @Column(name = "email")
-    private String email;
-
-    @Column(name = "className")
-    private String className;
+        @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+        @JoinTable(name = "t_user_role",
+                joinColumns = {@JoinColumn(name = "user_id")},
+                inverseJoinColumns = {@JoinColumn(name = "role_id")})
+        private Set<Role> roles = new HashSet<>();
 }
